882110ea893b36305b1d3e66a0be0e18f72d14de
davjh <dillonsdad91@gmail.com>
2022-09-16 02:01:09 -0400

    added parseInt to js file

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+
+
+var character = prompt("How many characters would you like in your password?");
+
+
+// for (character.length < 8) { 
+//   prompt ("Character length must be greater than 8 characters.")};
+// let x = document.numb.value
+
+
+
+
+
+
+  if (parseInt(character) < 8) { 
+    confirm ("Character length must be greater than 8 characters.");}
+
+    else if (parseInt(character) > 125) {
+    confirm("Character length must be less than 125 characters");}
+
+    
+}
+
+  // else if (character.valueOf.length > 125) { prompt ("Character length must be less than 125 characters.")};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return "generated password";
+
+// }
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,13 +62,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var character= prompt("How many characters would you like your password to be?");
 
-var lCase= prompt("Do you want lowercase letters included in your new password");
 
-var uCase= prompt("Do you want uppercase letters included in your new password");
 
-var nums= prompt("Do you want numbers included in your new password");
+// var lCase= confirm("Do you want lowercase letters included in your new password");
 
-var special= prompt("Do you want special characters included in your new password");
+// var uCase= confirm("Do you want uppercase letters included in your new password");
 
+// var nums= confirm("Do you want numbers included in your new password");
+
+// var special= confirm("Do you want special characters included in your new password");
